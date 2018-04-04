c9b03da00b85028860d08114b0f86626db2fd2c5
Raphael Wilker <Raphaew20161@gmail.com>
2018-04-04 02:11:21 -0300

    Start Projeto

---
+++ src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,10 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * nome do caminho do JNDI datasource do tomcat
+ * @author Raphael
+ *
+ */
+public class VariavelConexaoUtil {
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+}
